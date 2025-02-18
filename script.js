fbdc29c3d6a6348697a5313fa5e4198e850faf9c
yuzpopz <160543069+yuzpopz@users.noreply.github.com>
2025-02-18 21:49:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -154,3 +154,47 @@ document.addEventListener("mousemove", () => {
     showCursorAndControls();
     timeoutId = setTimeout(hideCursorAndControls, 4000);
 })
+
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) { /* Firefox */
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+            document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) { /* IE/Edge */
+            document.documentElement.msRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) { /* Firefox */
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE/Edge */
+            document.msExitFullscreen();
+        }
+    }
+}
+
+let tappedTwice = false;
+
+document.addEventListener('touchstart', function (event) {
+    if (event.touches.length === 1) {
+        if (tappedTwice) {
+            tappedTwice = false;
+            toggleFullScreen();
+        } else {
+            tappedTwice = true;
+            setTimeout(function () {
+                tappedTwice = false;
+            }, 300);
+        }
+    } else {
+        tappedTwice = false;
+    }
+});
+
+document.addEventListener('dblclick', toggleFullScreen);
